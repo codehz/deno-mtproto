b61425dc8f7203509ec14a803f6e7895d9e78c6b
codehz <codehz@outlook.com>
2022-08-18 00:20:59 +0800

    add bot login

---
+++ auth/bot.ts
@@ -0,0 +1,37 @@
+import MTProto from "mtproto/mod.ts";
+import parse_error from "mtproto/common/errparse.ts";
+import { RPCError } from "mtproto/rpc/mod.ts";
+
+export async function loginAsBot(proto: MTProto, token: string) {
+  while (true) {
+    const rpc = await proto.rpc();
+    let ret;
+    try {
+      ret = await rpc.api.auth.importBotAuthorization({
+        flags: 0,
+        bot_auth_token: token,
+      });
+    } catch (e) {
+      if (e instanceof RPCError) {
+        let mig_dc;
+        if (
+          (mig_dc = parse_error("USER_MIGRATE_", e.message)) != null
+        ) {
+          proto.default_dc = mig_dc;
+          continue;
+        } else if (e.message == "API_ID_INVALID") {
+          throw new Error("Invalid api id", { cause: e });
+        } else if (e.message == "API_ID_PUBLISHED_FLOOD") {
+          throw new Error("Invalid api id", { cause: e });
+        } else {
+          throw new Error("unknown error code", { cause: e });
+        }
+      }
+      throw e;
+    }
+    if (ret._ !== "auth.authorization") {
+      throw new Error(`Invalid response: ${ret._}`);
+    }
+    return ret;
+  }
+}
